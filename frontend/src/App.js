2b9964b22c365b872d5edfb70ac3ddb208b9bf4e
Malik Ali <mac@macs-MacBook-Pro.local>
2024-08-09 19:28:20 +0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from './components/Home';
+
+class App extends Component {
+  render(){
+    return(
+    <Router>
+      <Routes>
+        <Route path='/' Component={Home}>
+
+        </Route>
+      </Routes>
+    </Router>
+    )
+  }
+    
+  
+}
+
+export default App;
